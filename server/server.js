ef11cbf6aa540ab6384b4838f64db9620b1d1422
Seth Sutherland <cokolatepal@gmail.com>
2017-04-19 17:37:28 -0600

    working on the server

--- server/server.js
+++ server/server.js
@@ -2,15 +2,18 @@
  * Created by Seth on 4/19/2017.
  */
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var config = require('./config');
 
 var app = express();
 app.use(express.static(__dirname + '../public'));
+app.use(bodyParser.json());
+
 
 
 var port = config.PORT;
 app.listen(port, function(){
-   console.log('Listening on ' + port);
+   console.log('Listening on port ' + port);
 });
 
