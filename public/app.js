84035f53810735128a728c87a3711cc4c1889dc8
Seth Sutherland <cokolatepal@gmail.com>
2017-04-20 16:42:05 -0600

    adjustments

--- public/app.js
+++ public/app.js
@@ -8,6 +8,26 @@ angular.module('myApp', ['ui.router'])
                 url: '/',
                 templateUrl: 'home.html',
                 controller: 'mainCtrl'
+            })
+            .state('portfolio', {
+                url: '/portfolio',
+                templateUrl: 'portfolio.html',
+                controller: 'portCtrl'
+            })
+            .state('resume', {
+                url: '/resume',
+                templateUrl: 'resume.html',
+                controller: 'resumeCtrl'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'about.html',
+                controller: 'contact'
+            })
+            .state('contact', {
+                url: '/contact',
+                templateUrl: 'contact.html',
+                controller: 'contactCtrl'
             });
 
 });
