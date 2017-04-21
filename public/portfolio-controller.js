bc548995ab0beaf59991cf18446e4d00d4534af3
Seth Sutherland <cokolatepal@gmail.com>
2017-04-21 14:25:32 -0600

    portfolio

--- public/portfolio-controller.js
+++ public/portfolio-controller.js
@@ -8,6 +8,7 @@ angular.module('myApp').controller('portfolioCtrl', function($scope, mainService
         situation: 'I needed an app that would allow me to add and save Google Maps locations, upload and locally save pictures and then present them again in an easy to navigate way.',
         action: 'I used Angular to create my app',
         result: 'VentureBound was the result',
-        tech: 'HTML/CSS, JavaScript, AngularJS, jQuery, TweenMax, NodeJS, Massive, Express, Body-Parser, Local Authentication, PostgreSQL.'
+        tech: 'HTML/CSS, JavaScript, AngularJS, jQuery, TweenMax, NodeJS, Massive, Express, Body-Parser, Local Authentication, PostgreSQL.',
+        screenshot: './screenshot.png'
     }
 });
