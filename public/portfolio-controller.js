ea76d38ae73c79242d5788fa0e1be0545ddcb353
Seth Sutherland <cokolatepal@gmail.com>
2017-04-21 12:34:44 -0600

    working on the nav for the projects

--- public/portfolio-controller.js
+++ public/portfolio-controller.js
@@ -3,5 +3,11 @@
  */
 angular.module('myApp').controller('portfolioCtrl', function($scope, mainService){
 
-    $scope.projectInfo = 'VentureBound';
+    $scope.project = {
+        name: 'VentureBound',
+        situation: 'I needed an app that would allow me to add and save Google Maps locations, upload and locally save pictures and then present them again in an easy to navigate way.',
+        action: 'I used Angular to create my app',
+        result: 'VentureBound was the result',
+        tech: 'HTML/CSS, JavaScript, AngularJS, jQuery, TweenMax, NodeJS, Massive, Express, Body-Parser, Local Authentication, PostgreSQL.'
+    }
 });
