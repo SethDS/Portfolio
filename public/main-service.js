35c98e65812d0b72594b5f13072065494c0b4acd
Seth Sutherland <cokolatepal@gmail.com>
2017-04-21 16:15:13 -0600

    portfolio

--- public/main-service.js
+++ public/main-service.js
@@ -3,4 +3,46 @@
  */
 angular.module('myApp').service('mainService', function () {
 
+    this.projects = [
+        {
+            name: 'VentureBound',
+            situation: 'I needed an app that would allow me to add and save Google Maps locations, upload and locally save pictures and then present them again in an easy to navigate way.',
+            action: 'I used Angular to create my app',
+            result: 'VentureBound was the result',
+            tech: 'HTML/CSS, JavaScript, AngularJS, jQuery, TweenMax, NodeJS, Massive, Express, Body-Parser, Local Authentication, PostgreSQL.',
+            screenshot: './screenshot.png'
+        },
+        {
+            name: 'Top Secret Clone',
+            situation: 'I needed an app that would allow me to add and save Google Maps locations, upload and locally save pictures and then present them again in an easy to navigate way.',
+            action: 'I used Angular to create my app',
+            result: 'VentureBound was the result',
+            tech: 'HTML/CSS, JavaScript, AngularJS, jQuery, TweenMax, NodeJS, Massive, Express, Body-Parser, Local Authentication, PostgreSQL.',
+            screenshot: './screenshot.png'
+        },
+        {
+            name: 'Bound Books Clone',
+            situation: 'I needed an app that would allow me to add and save Google Maps locations, upload and locally save pictures and then present them again in an easy to navigate way.',
+            action: 'I used Angular to create my app',
+            result: 'VentureBound was the result',
+            tech: 'HTML/CSS, JavaScript, AngularJS, jQuery, TweenMax, NodeJS, Massive, Express, Body-Parser, Local Authentication, PostgreSQL.',
+            screenshot: './screenshot.png'
+        },
+        {
+            name: 'Vacheron Constantin Clone',
+            situation: 'I needed an app that would allow me to add and save Google Maps locations, upload and locally save pictures and then present them again in an easy to navigate way.',
+            action: 'I used Angular to create my app',
+            result: 'VentureBound was the result',
+            tech: 'HTML/CSS, JavaScript, AngularJS, jQuery, TweenMax, NodeJS, Massive, Express, Body-Parser, Local Authentication, PostgreSQL.',
+            screenshot: './screenshot.png'
+        },
+        {
+            name: 'Famiglia Cecchi Clone',
+            situation: 'I needed an app that would allow me to add and save Google Maps locations, upload and locally save pictures and then present them again in an easy to navigate way.',
+            action: 'I used Angular to create my app',
+            result: 'VentureBound was the result',
+            tech: 'HTML/CSS, JavaScript, AngularJS, jQuery, TweenMax, NodeJS, Massive, Express, Body-Parser, Local Authentication, PostgreSQL.',
+            screenshot: './screenshot.png'
+        }
+    ];
 });
