f3d884735a695960ad779ee3a195aaf93ed43365
Seth Sutherland <cokolatepal@gmail.com>
2017-04-24 15:08:47 -0600

    almost done with portfolio view.

--- public/portfolio-controller.js
+++ public/portfolio-controller.js
@@ -5,22 +5,39 @@ angular.module('myApp').controller('portfolioCtrl', function($scope, mainService
 
    $scope.projects = mainService.projects;
    var count = 0;
+    var theElement = document.getElementById('project-' + $scope.projects[count].id);
+    $(theElement).css({
+        "transform": "rotate(90deg)"
+    });
    $scope.project = $scope.projects[0];
 
    $scope.pageTurnRight = function(){
+
       if(count < $scope.projects.length - 1){
+          $(theElement).css({
+              "transform": "rotate(0deg)"
+          });
          count++;
           $scope.project = $scope.projects[count];
-          var theElement = document.getElementById('project-' + $scope.projects[count].id);
-          theElement.css('color', 'white');
+          theElement = document.getElementById('project-' + $scope.projects[count].id);
+         $(theElement).css({
+            "transform": "rotate(90deg)"
+         })
       }
    };
    $scope.pageTurnLeft = function(){
+
       if(count > 0){
+          $(theElement).css({
+              "transform": "rotate(0deg)"
+          });
          count--;
          $scope.project = $scope.projects[count];
-          var theElement = document.getElementById('project-' + $scope.projects[count].id);
-          theElement.css('color', 'white');
+          theElement = document.getElementById('project-' + $scope.projects[count].id);
+         console.log(theElement);
+          $(theElement).css({
+              "transform": "rotate(90deg)"
+          })
       }
    }
 
