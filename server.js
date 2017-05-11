d64e2c714272cfeb94aef939de64b9a4a6a75d1e
Seth Sutherland <cokolatepal@gmail.com>
2017-05-11 14:39:57 -0600

    hosting in progress

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var config = require('./config');
+var config = require('./server/config');
 
 var app = express();
 app.use(express.static('public'));
